c071dccfe0b10ae68e75c9f089f81bd56c07cc2c
ken <eugenekennyjensen@gmail.com>
2025-10-19 15:44:39 -0400

    Switch copilot.lua to CopilotChat.nvim

--- init.lua
+++ init.lua
@@ -1143,90 +1143,55 @@ require('lazy').setup({
     end,
   },
   {
-    'zbirenbaum/copilot.lua',
+    'CopilotC-Nvim/CopilotChat.nvim',
     version = '*',
-    event = 'InsertEnter',
-    cmd = 'Copilot',
-    requires = {
-      'copilotlsp-nvim/copilot-lsp', -- (optional) for NES functionality
+    event = 'VimEnter',
+    dependencies = {
+      { 'nvim-lua/plenary.nvim', branch = 'master' },
     },
+    opts = {
+      window = {
+        layout = 'float',
+        border = 'shadow',
+      },
+      mappings = {
+        accept = nil,
+        complete = nil,
+        close = nil,
+        reset = nil,
+        submit_prompt = nil,
+        toggle_sticky = nil,
+        accept_diff = nil,
+        jump_to_diff = nil,
+        quickfix_diffs = nil,
+        yank_diff = nil,
+        show_diff = nil,
+        show_info = nil,
+        show_help = nil,
+      },
+    },
+
     config = function()
-      -- The following options are the defaults
-      require('copilot').setup {
-        panel = {
-          enabled = true,
-          auto_refresh = false,
-          keymap = {
-            jump_prev = '[[',
-            jump_next = ']]',
-            accept = '<CR>',
-            refresh = 'gr',
-            open = '<M-CR>',
-          },
-          layout = {
-            position = 'bottom', -- | top | left | right | bottom |
-            ratio = 0.4,
-          },
-        },
-        suggestion = {
-          enabled = true,
-          auto_trigger = false,
-          hide_during_completion = true,
-          debounce = 75,
-          trigger_on_accept = true,
-          keymap = {
-            accept = '<M-l>',
-            accept_word = false,
-            accept_line = false,
-            next = '<M-]>',
-            prev = '<M-[>',
-            dismiss = '<C-]>',
-          },
-        },
-        nes = {
-          enabled = false, -- requires copilot-lsp as a dependency
-          auto_trigger = false,
-          keymap = {
-            accept_and_goto = false,
-            accept = false,
-            dismiss = false,
-          },
-        },
-        auth_provider_url = nil, -- URL to authentication provider, if not "https://github.com/"
-        logger = {
-          file = vim.fn.stdpath 'log' .. '/copilot-lua.log',
-          file_log_level = vim.log.levels.OFF,
-          print_log_level = vim.log.levels.WARN,
-          trace_lsp = 'off', -- "off" | "messages" | "verbose"
-          trace_lsp_progress = false,
-          log_lsp_messages = false,
-        },
-        copilot_node_command = 'node', -- Node.js version must be > 22
-        workspace_folders = {},
-        copilot_model = '',
-        disable_limit_reached_message = false, -- Set to `true` to suppress completion limit reached popup
-        root_dir = function()
-          return vim.fs.dirname(vim.fs.find('.git', { upward = true })[1])
-        end,
-        should_attach = function(_, _)
-          if not vim.bo.buflisted then
-            logger.debug "not attaching, buffer is not 'buflisted'"
-            return false
-          end
+      local cpf = require 'CopilotChat'
 
-          if vim.bo.buftype ~= '' then
-            logger.debug("not attaching, buffer 'buftype' is " .. vim.bo.buftype)
-            return false
-          end
+      local map = function(keys, func, desc, opts, mode)
+        mode = mode or 'n'
 
-          return true
-        end,
-        server = {
-          type = 'nodejs', -- "nodejs" | "binary"
-          custom_server_filepath = nil,
-        },
-        server_opts_overrides = {},
-      }
+        if not opts then
+          opts = {}
+        end
+
+        opts.desc = 'CopilotChat: ' .. desc
+
+        vim.keymap.set(mode, keys, func, opts)
+      end
+
+      local myToggle = function()
+        cpf.toggle(cpf.config)
+      end
+
+      map('<leader>cc', myToggle, 'Toggle [C]opilot chat')
+      map('<leader>cb', cpf.ask, 'Toggle [C]opilot chat') -- TODO: add the current buffer to the prompt
     end,
   },
 
